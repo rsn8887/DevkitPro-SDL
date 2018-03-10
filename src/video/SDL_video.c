7ca1ff2c7d5615fe58793396b585fb1ae4aef33a
Cpasjuste <cpasjuste@gmail.com>
2018-03-10 22:02:38 +0100

    SWITCH: first commit, missing audio, input... (#1)
    * SWITCH: first commit, missing audio, input...

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -138,6 +138,9 @@ static VideoBootStrap *bootstrap[] = {
 #if SDL_VIDEO_DRIVER_OFFSCREEN
     &OFFSCREEN_bootstrap,
 #endif
+#if SDL_VIDEO_DRIVER_SWITCH
+    &SWITCH_bootstrap,
+#endif
 #if SDL_VIDEO_DRIVER_DUMMY
     &DUMMY_bootstrap,
 #if SDL_INPUT_LINUXEV
