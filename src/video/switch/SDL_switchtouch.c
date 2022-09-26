e63f0a90fa1af7bf94ae961b52bf62e748d7eeee
Wohlstand <admin@wohlnet.ru>
2022-09-26 05:56:32 +0300

    SDL_switchtouch.c: Fixed the touchscreen work - Rewrite the code with a more optimal - ID should be always 1, if id 0, means it's an invalid device

--- src/video/switch/SDL_switchtouch.c
+++ src/video/switch/SDL_switchtouch.c
@@ -29,59 +29,89 @@
 #include "../../events/SDL_touch_c.h"
 #include "../../video/SDL_sysvideo.h"
 
-HidTouchScreenState touchState;
-HidTouchScreenState touchStateOld;
+static HidTouchScreenState touchState;
+static HidTouchScreenState touchStateOld;
 
-void
-SWITCH_InitTouch(void) {
+void SWITCH_InitTouch(void)
+{
     hidInitializeTouchScreen();
     SDL_AddTouch((SDL_TouchID) 0, SDL_TOUCH_DEVICE_DIRECT, "Switch");
     SDL_SetHintWithPriority(SDL_HINT_TOUCH_MOUSE_EVENTS, "0", SDL_HINT_DEFAULT);
+    SDL_memset(&touchState, 0, sizeof(HidTouchScreenState));
+    SDL_memset(&touchStateOld, 0, sizeof(HidTouchScreenState));
 }
 
-void
-SWITCH_QuitTouch(void) {
-
+void SWITCH_QuitTouch(void)
+{
 }
 
-void
-SWITCH_PollTouch(void) {
+void SWITCH_PollTouch(void)
+{
+    const float rel_w = 1280.0f, rel_h = 720.0f;
     SDL_Window *window = SDL_GetFocusWindow();
-    if (window == NULL) {
+    SDL_TouchID id = 1;
+    SDL_bool found;
+    s32 i, j;
+
+    if (!window) {
         return;
     }
 
-    memcpy(&touchStateOld, &touchState, sizeof(touchState));
+    if (SDL_AddTouch(id, SDL_TOUCH_DEVICE_DIRECT, "") < 0) {
+        SDL_Log("error: can't add touch %s, %d", __FILE__, __LINE__);
+    }
+
+    SDL_memcpy(&touchStateOld, &touchState, sizeof(touchState));
 
     if (hidGetTouchScreenStates(&touchState, 1)) {
-        for (u32 i = 0; i < touchState.count; i++) {
-            // Send an initial touch
-            SDL_SendTouch(0, touchState.touches[i].finger_id, window, SDL_TRUE,
-                          (float) touchState.touches[i].x / 1280.0f,
-                          (float) touchState.touches[i].y / 720.0f, 1);
-            // Always send the motion
-            SDL_SendTouchMotion(0, (SDL_FingerID) i, window,
-                                (float) touchState.touches[i].x / 1280.0f,
-                                (float) touchState.touches[i].y / 720.0f, 1);
-        }
+        /* Finger down */
+        if (touchStateOld.count < touchState.count) {
+            for (i = 0; i < touchState.count; i++) {
+                found = SDL_FALSE;
 
-        // some fingers might have been let go
-        if (touchStateOld.count > 0) {
-            for (int i = 0; i < touchStateOld.count; i++) {
-                int finger_up = 1;
-                if (touchState.count > 0) {
-                    for (int j = 0; j < touchState.count; j++) {
-                        if (touchState.touches[j].finger_id == touchStateOld.touches[i].finger_id) {
-                            finger_up = 0;
-                        }
+                for (j = 0; j < touchStateOld.count; j++) {
+                    if (touchStateOld.touches[j].finger_id == touchState.touches[i].finger_id) {
+                        found = SDL_TRUE;
+                        break;
                     }
                 }
-                if (finger_up == 1) {
-                    // Finger released from screen
-                    SDL_SendTouch((SDL_TouchID) 0, (SDL_FingerID) touchStateOld.touches[i].finger_id, window, SDL_FALSE,
-                                  (float) touchStateOld.touches[i].x / 1280.0f,
-                                  (float) touchStateOld.touches[i].y / 720.0f, 1);
+
+                if (!found) {
+                    SDL_SendTouch(id,
+                                  touchState.touches[i].finger_id,
+                                  window, SDL_TRUE,
+                                  (float)touchState.touches[i].x / rel_w,
+                                  (float)touchState.touches[i].y / rel_h, 1);
                 }
+            }
+        }
+
+        /* Scan for moves or up */
+        for (i = 0; i < touchStateOld.count; i++) {
+            found = SDL_FALSE;
+
+            for (j = 0; j < touchState.count; j++) {
+                if (touchState.touches[j].finger_id == touchStateOld.touches[i].finger_id) {
+                    found = SDL_TRUE;
+                    /* Finger moved */
+                    if (touchState.touches[j].x != touchStateOld.touches[i].x || touchState.touches[j].y != touchStateOld.touches[i].y) {
+                        SDL_SendTouchMotion(id,
+                                            (SDL_FingerID)i, window,
+                                            (float)touchState.touches[j].x / rel_w,
+                                            (float)touchState.touches[j].y / rel_h, 1);
+                    }
+                    break;
+                }
+            }
+
+            if (!found) {
+                /* Finger Up */
+                SDL_SendTouch(id,
+                              (SDL_FingerID)touchStateOld.touches[i].finger_id,
+                              window,
+                              SDL_FALSE,
+                              (float) touchStateOld.touches[i].x / rel_w,
+                              (float) touchStateOld.touches[i].y / rel_h, 1);
             }
         }
     }
